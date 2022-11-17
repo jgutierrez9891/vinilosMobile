fc51bdd0124415242e1e6238855a2130265d016b
dchala8 <d.chala@uniandes.edu.co>
2022-11-17 07:41:08 -0500

    Se construye la lupa de busqueda

--- app/src/main/java/com/example/android/vinylsappg21/ui/collectors/CollectorsFragment.kt
+++ app/src/main/java/com/example/android/vinylsappg21/ui/collectors/CollectorsFragment.kt
@@ -50,6 +50,22 @@ class CollectorsFragment : Fragment() {
         recyclerView.layoutManager = GridLayoutManager(context,2)
         recyclerView.adapter = viewModelAdapter
 
+        binding.searchTextCollector.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                if (::artistsList.isInitialized) {
+                    viewModelAdapter!!.collectors =
+                        artistsList.filter { it.name.contains(s, ignoreCase = true) }
+                }
+            }
+        })
 
     }
 
