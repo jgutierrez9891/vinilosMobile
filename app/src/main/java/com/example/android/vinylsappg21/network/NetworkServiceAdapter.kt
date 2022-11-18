49193f800e0eea560bc25a115fa7366a6efab3e4
jgutierrez9891 <j.gutierrezl@uniandes.edu.co>
2022-11-18 01:03:03 -0500

    Implementación detalle de artista

--- app/src/main/java/com/example/android/vinylsappg21/network/NetworkServiceAdapter.kt
+++ app/src/main/java/com/example/android/vinylsappg21/network/NetworkServiceAdapter.kt
@@ -11,9 +11,8 @@ import com.android.volley.toolbox.Volley
 import com.example.android.vinylsappg21.models.Album
 import com.example.android.vinylsappg21.models.Artist
 import org.json.JSONArray
+import org.json.JSONException
 import org.json.JSONObject
-import java.util.*
-import kotlin.Comparator
 
 class NetworkServiceAdapter constructor(context: Context) {
     companion object{
@@ -52,9 +51,14 @@ class NetworkServiceAdapter constructor(context: Context) {
             Response.Listener<String> { response ->
                 val resp = JSONArray(response)
                 val list = mutableListOf<Artist>()
+                var albumsList = arrayListOf<String>()
                 for (i in 0 until resp.length()) {
                     val item = resp.getJSONObject(i)
-                    list.add(i, Artist(artistId = item.getInt("id"),name = item.getString("name"), image = item.getString("image"), description = item.getString("description")))
+                    if (item.getJSONArray("albums").length()>0){
+                        val jsonArray = item.getJSONArray("albums")
+                        albumsList = jsonArray.toArrayList()
+                    }
+                    list.add(i, Artist(artistId = item.getInt("id"),name = item.getString("name"), image = item.getString("image"), description = item.getString("description"), birthday = item.getString("birthDate"), albums = albumsList))
                 }
                 list.sortBy{it.name?.toString()}
                 onComplete(list)
@@ -62,6 +66,14 @@ class NetworkServiceAdapter constructor(context: Context) {
             Response.ErrorListener {
                 onError(it)
             }))
+    }
+
+    fun JSONArray.toArrayList(): ArrayList<String> {
+        val list = arrayListOf<String>()
+        for (i in 0 until this.length()) {
+            list.add(this.getString(i))
+        }
+        return list
     }
 
     private fun getRequest(path:String, responseListener: Response.Listener<String>, errorListener: Response.ErrorListener): StringRequest {
