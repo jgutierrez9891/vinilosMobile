873656d6d210f29a3ba9f0ed3540b997618060da
CharlyMoraUA <ca.morar1@uniandes.edu.co>
2022-11-20 18:29:58 -0500

    corrutinas en artista

--- app/src/main/java/com/example/android/vinylsappg21/network/CacheManager.kt
+++ app/src/main/java/com/example/android/vinylsappg21/network/CacheManager.kt
@@ -1,6 +1,7 @@
 package com.example.android.vinylsappg21.network
 import android.content.Context
 import com.example.android.vinylsappg21.models.Collector
+import com.example.android.vinylsappg21.models.Artist
 
 class CacheManager(context: Context) {
     companion object{
@@ -13,6 +14,12 @@ class CacheManager(context: Context) {
             }
     }
     private var collectors: List<Collector> = listOf()
+    private var artists: List<Artist> = listOf()
+    fun addArtists(artistList: List<Artist>){
+        if (artists.isEmpty() || artistList.size != artists.size){
+            artists = artistList
+        }
+    }
     fun addCollectors(collectorList: List<Collector>){
         if (collectors.isEmpty() || collectorList.size != collectors.size){
                 collectors = collectorList
@@ -20,5 +27,8 @@ class CacheManager(context: Context) {
     }
     fun getCollectors() : List<Collector>{
         return if (collectors.isNotEmpty()) collectors!! else listOf<Collector>()
+    }
+    fun getArtists() : List<Artist>{
+        return if (artists.isNotEmpty()) artists!! else listOf<Artist>()
     }
 }
--- app/src/main/java/com/example/android/vinylsappg21/repositories/CollectorsRepository.kt
+++ app/src/main/java/com/example/android/vinylsappg21/repositories/CollectorsRepository.kt
@@ -1,6 +1,5 @@
 package com.example.android.vinylsappg21.repositories
 
-
 import android.app.Application
 import android.util.Log
 import com.android.volley.VolleyError
