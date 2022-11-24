13f9297301e5a529d3cdcb47a198381a058a9763
jgutierrez9891 <j.gutierrezl@uniandes.edu.co>
2022-11-23 23:51:26 -0500

    Creacion actividad detalle coleccionista, fix de albumes en detalle artista

--- app/src/main/java/com/example/android/vinylsappg21/ui/adapters/CollectorsAdapter.kt
+++ app/src/main/java/com/example/android/vinylsappg21/ui/adapters/CollectorsAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.android.vinylsappg21.ui.adapters
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.vinylsappg21.R
 import com.example.android.vinylsappg21.databinding.CollectorItemBinding
 import com.example.android.vinylsappg21.models.Collector
+import com.example.android.vinylsappg21.ui.collectors.CollectorDetailActivity
 
 class CollectorsAdapter : RecyclerView.Adapter<CollectorsAdapter.CollectorsViewHolder>(){
 
@@ -29,6 +32,18 @@ class CollectorsAdapter : RecyclerView.Adapter<CollectorsAdapter.CollectorsViewH
     override fun onBindViewHolder(holder: CollectorsViewHolder, position: Int) {
         holder.viewDataBinding.also {
             it.collector = collectors[position]
+        }
+
+        holder.itemView.setOnClickListener { v ->
+            val activity = v!!.context as AppCompatActivity;
+            activity?.let{
+                val intent = Intent (it, CollectorDetailActivity::class.java)
+                intent.putExtra("collector_name", collectors[position].name)
+                intent.putExtra("collector_tel", collectors[position].telephone)
+                intent.putExtra("collector_email", collectors[position].email)
+                intent.putStringArrayListExtra("collector_albums", arrayListOf(*collectors[position].collectorAlbums))
+                it.startActivity(intent)
+            }
         }
     }
 
