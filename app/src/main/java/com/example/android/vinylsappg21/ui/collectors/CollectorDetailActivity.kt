e9c918c20a8b82bab85fae077f1f5af9d8b90e79
jgutierrez9891 <j.gutierrezl@uniandes.edu.co>
2022-12-04 13:29:42 -0500

    Ajustes por microoptimizaciones

--- app/src/main/java/com/example/android/vinylsappg21/ui/collectors/CollectorDetailActivity.kt
+++ app/src/main/java/com/example/android/vinylsappg21/ui/collectors/CollectorDetailActivity.kt
@@ -6,7 +6,6 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
 import com.example.android.vinylsappg21.R
-import com.example.android.vinylsappg21.models.Collector
 
 class CollectorDetailActivity : AppCompatActivity() {
 
